9380aa62914f7ba911ab7c3a310e73a876235101
radoslaw-cybulski <radek199317@wp.pl>
2018-12-12 19:11:40 +0100

    Add files via upload
    dodanie akcji w dół

--- bellman.py
+++ bellman.py
@@ -37,7 +37,7 @@ for przejscie in range(1):
         for kolumna in range(rozmiarmapy[0]):
             if mapatypow[wiersz][kolumna] == 1:
                 print(wiersz, kolumna)
-                #licze ruch do góry
+                #licze akcje ruch do góry
                 #if wiersz == 0 or mapatypow[wiersz-1][kolumna] == 0:
                 #    ruch_do_gory = 0.8 * Vstary[wiersz][kolumna]
                 #else:
@@ -52,20 +52,37 @@ for przejscie in range(1):
                 #    przemieszczenia_w_prawo = 0.1 * Vstary[wiersz][kolumna + 1]
                 #akcja_gora = ruch_do_gory + przemieszczenia_w_lewo + przemieszczenia_w_prawo
                 #print(akcja_gora)
-                if kolumna == rozmiarmapy[0]-1 or mapatypow[wiersz][kolumna + 1] == 0:
-                    ruch_w_prawo = 0.8 * Vstary[wiersz][kolumna]
-                else:
-                    ruch_w_prawo = 0.8 * Vstary[wiersz][kolumna + 1]
-                if wiersz == 0 or mapatypow[wiersz - 1][kolumna] == 0:
-                    przemieszczenie_w_gore = 0.1 * Vstary[wiersz][kolumna]
-                else:
-                    przemieszczenie_w_gore = 0.1 * Vstary[wiersz - 1][kolumna]
+                #licze akcje ruch w prawo
+                #if kolumna == rozmiarmapy[0]-1 or mapatypow[wiersz][kolumna + 1] == 0:
+                #    ruch_w_prawo = 0.8 * Vstary[wiersz][kolumna]
+                #else:
+                #    ruch_w_prawo = 0.8 * Vstary[wiersz][kolumna + 1]
+                #if wiersz == 0 or mapatypow[wiersz - 1][kolumna] == 0:
+                #    przemieszczenie_w_gore = 0.1 * Vstary[wiersz][kolumna]
+                #else:
+                #    przemieszczenie_w_gore = 0.1 * Vstary[wiersz - 1][kolumna]
+                #if wiersz == rozmiarmapy[1]-1 or mapatypow[wiersz + 1][kolumna] == 0:
+                #    przemieszczenie_w_dol = 0.1 * Vstary[wiersz][kolumna]
+                #else:
+                #    przemieszczenie_w_dol = 0.1 * Vstary[wiersz + 1][kolumna]
+                #akcja_prawo = ruch_w_prawo + przemieszczenie_w_gore + przemieszczenie_w_dol
+                #print(akcja_prawo)
+                #licze akcje ruchu w dół
                 if wiersz == rozmiarmapy[1]-1 or mapatypow[wiersz + 1][kolumna] == 0:
-                    przemieszczenie_w_dol = 0.1 * Vstary[wiersz][kolumna]
+                    ruch_w_dol = 0.8 * Vstary[wiersz][kolumna]
                 else:
-                    przemieszczenie_w_dol = 0.1 * Vstary[wiersz + 1][kolumna]
-                akcja_prawo = ruch_w_prawo + przemieszczenie_w_gore + przemieszczenie_w_dol
-                print(akcja_prawo)
+                    ruch_w_dol = 0.8 * Vstary[wiersz + 1][kolumna]
+                if kolumna == 0 or mapatypow[wiersz][kolumna - 1] == 0:
+                    przemieszczenie_w_lewo = 0.1 * Vstary[wiersz][kolumna]
+                else:
+                    przemieszczenie_w_lewo = 0.1 * Vstary[wiersz][kolumna - 1]
+                if kolumna == rozmiarmapy[0]-1 or mapatypow[wiersz][kolumna + 1] == 0:
+                    przemieszczenie_sie_w_prawo = 0.1 * Vstary[wiersz][kolumna]
+                else:
+                    przemieszczenie_sie_w_prawo = 0.1 * Vstary[wiersz][kolumna + 1]
+                akcja_dol = ruch_w_dol + przemieszczenie_w_lewo + przemieszczenie_sie_w_prawo
+                print(akcja_dol)
+
 
 
 
