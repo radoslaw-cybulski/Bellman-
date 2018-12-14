5eeab7edf5593717f21544d024c9fd1e01292a51
radoslaw-cybulski <radek199317@wp.pl>
2018-12-14 22:59:05 +0100

    Add files via upload
    do refaktoryzacji

--- bellman.py
+++ bellman.py
@@ -9,11 +9,19 @@ gama = 0.5
 V = []
 Vstary = []
 listaruchow = []
+politykaruchu = []
+
 
 for word in f.readline().split():
     rozmiarmapy.append(int(word))
 
 print(rozmiarmapy)
+
+for wiersz in range(rozmiarmapy[1] ):
+    politykaruchu.append([0])
+    for kolumna in range(rozmiarmapy[0] - 1):
+        politykaruchu[wiersz].append(0)
+print (politykaruchu)
 
 for linia in range(rozmiarmapy[1]):
     mapatypow.append(f.readline().split())
@@ -31,61 +39,62 @@ for i in range(rozmiarmapy[1]):
 
 V = deepcopy(mapakosztow)
 
-for przejscie in range(1):
+for przejscie in range(100):
     Vstary = deepcopy(V)
     for wiersz in range(rozmiarmapy[1]):
         for kolumna in range(rozmiarmapy[0]):
             if mapatypow[wiersz][kolumna] == 1:
                 print(wiersz, kolumna)
                 #licze akcje ruch do góry
-                #if wiersz == 0 or mapatypow[wiersz-1][kolumna] == 0:
-                #    ruch_do_gory = 0.8 * Vstary[wiersz][kolumna]
-                #else:
-                #    ruch_do_gory = 0.8 * Vstary[wiersz-1][kolumna]
-                #if kolumna == 0 or mapatypow[wiersz][kolumna-1] == 0:
-                #    przemieszczenia_w_lewo = 0.1 * Vstary[wiersz][kolumna]
-                #else:
-                #    przemieszczenia_w_lewo = 0.1 * Vstary[wiersz][kolumna-1]
-                #if kolumna == rozmiarmapy[0]-1 or mapatypow[wiersz][kolumna+1] == 0:
-                #    przemieszczenia_w_prawo = 0.1 * Vstary[wiersz][kolumna]
-                #else:
-                #    przemieszczenia_w_prawo = 0.1 * Vstary[wiersz][kolumna + 1]
-                #akcja_gora = ruch_do_gory + przemieszczenia_w_lewo + przemieszczenia_w_prawo
+                if wiersz == 0 or mapatypow[wiersz-1][kolumna] == 0:
+                    ruch_do_gory = 0.8 * Vstary[wiersz][kolumna]
+                else:
+                    ruch_do_gory = 0.8 * Vstary[wiersz-1][kolumna]
+                if kolumna == 0 or mapatypow[wiersz][kolumna-1] == 0:
+                    przemieszczenie_w_lewo = 0.1 * Vstary[wiersz][kolumna]
+                else:
+                    przemieszczenie_w_lewo = 0.1 * Vstary[wiersz][kolumna-1]
+                if kolumna == rozmiarmapy[0]-1 or mapatypow[wiersz][kolumna+1] == 0:
+                    przemieszczenie_w_prawo = 0.1 * Vstary[wiersz][kolumna]
+                else:
+                    przemieszczenie_w_prawo = 0.1 * Vstary[wiersz][kolumna + 1]
+                akcja_gora = ruch_do_gory + przemieszczenie_w_lewo + przemieszczenie_w_prawo
                 #print(akcja_gora)
                 #licze akcje ruch w prawo
-                #if kolumna == rozmiarmapy[0]-1 or mapatypow[wiersz][kolumna + 1] == 0:
-                #    ruch_w_prawo = 0.8 * Vstary[wiersz][kolumna]
-                #else:
-                #    ruch_w_prawo = 0.8 * Vstary[wiersz][kolumna + 1]
-                #if wiersz == 0 or mapatypow[wiersz - 1][kolumna] == 0:
-                #    przemieszczenie_w_gore = 0.1 * Vstary[wiersz][kolumna]
-                #else:
-                #    przemieszczenie_w_gore = 0.1 * Vstary[wiersz - 1][kolumna]
-                #if wiersz == rozmiarmapy[1]-1 or mapatypow[wiersz + 1][kolumna] == 0:
-                #    przemieszczenie_w_dol = 0.1 * Vstary[wiersz][kolumna]
-                #else:
-                #    przemieszczenie_w_dol = 0.1 * Vstary[wiersz + 1][kolumna]
-                #akcja_prawo = ruch_w_prawo + przemieszczenie_w_gore + przemieszczenie_w_dol
+                if kolumna == rozmiarmapy[0]-1 or mapatypow[wiersz][kolumna + 1] == 0:
+                    ruch_w_prawo = 0.8 * Vstary[wiersz][kolumna]
+                else:
+                    ruch_w_prawo = 0.8 * Vstary[wiersz][kolumna + 1]
+                if wiersz == 0 or mapatypow[wiersz - 1][kolumna] == 0:
+                    przemieszczenie_w_gore = 0.1 * Vstary[wiersz][kolumna]
+                else:
+                    przemieszczenie_w_gore = 0.1 * Vstary[wiersz - 1][kolumna]
+                if wiersz == rozmiarmapy[1]-1 or mapatypow[wiersz + 1][kolumna] == 0:
+                    przemieszczenie_w_dol = 0.1 * Vstary[wiersz][kolumna]
+                else:
+                    przemieszczenie_w_dol = 0.1 * Vstary[wiersz + 1][kolumna]
+                akcja_prawo = ruch_w_prawo + przemieszczenie_w_gore + przemieszczenie_w_dol
                 #print(akcja_prawo)
                 #licze akcje ruchu w dół
-                #if wiersz == rozmiarmapy[1]-1 or mapatypow[wiersz + 1][kolumna] == 0:
-                #    ruch_w_dol = 0.8 * Vstary[wiersz][kolumna]
-                #else:
-                #    ruch_w_dol = 0.8 * Vstary[wiersz + 1][kolumna]
-                #if kolumna == 0 or mapatypow[wiersz][kolumna - 1] == 0:
-                #    przemieszczenie_w_lewo = 0.1 * Vstary[wiersz][kolumna]
-                #else:
-                #    przemieszczenie_w_lewo = 0.1 * Vstary[wiersz][kolumna - 1]
-                #if kolumna == rozmiarmapy[0]-1 or mapatypow[wiersz][kolumna + 1] == 0:
-                #    przemieszczenie_sie_w_prawo = 0.1 * Vstary[wiersz][kolumna]
-                #else:
-                #    przemieszczenie_sie_w_prawo = 0.1 * Vstary[wiersz][kolumna + 1]
-                #akcja_dol = ruch_w_dol + przemieszczenie_w_lewo + przemieszczenie_sie_w_prawo
+                if wiersz == rozmiarmapy[1]-1 or mapatypow[wiersz + 1][kolumna] == 0:
+                    ruch_w_dol = 0.8 * Vstary[wiersz][kolumna]
+                else:
+                    ruch_w_dol = 0.8 * Vstary[wiersz + 1][kolumna]
+                if kolumna == 0 or mapatypow[wiersz][kolumna - 1] == 0:
+                    przemieszczenie_w_lewo = 0.1 * Vstary[wiersz][kolumna]
+                else:
+                    przemieszczenie_w_lewo = 0.1 * Vstary[wiersz][kolumna - 1]
+                if kolumna == rozmiarmapy[0]-1 or mapatypow[wiersz][kolumna + 1] == 0:
+                    przemieszczenie_w_prawo = 0.1 * Vstary[wiersz][kolumna]
+                else:
+                    przemieszczenie_w_prawo = 0.1 * Vstary[wiersz][kolumna + 1]
+                akcja_dol = ruch_w_dol + przemieszczenie_w_lewo + przemieszczenie_w_prawo
                 #print(akcja_dol)
+                #licze akcje w lewo
                 if kolumna == 0 or mapatypow[wiersz][kolumna - 1] == 0:
                     ruch_w_lewo = 0.8 * Vstary[wiersz][kolumna]
                 else:
-                    ruch_w_lewo = 0.8 * Vstary[wiersz - 1][kolumna]
+                    ruch_w_lewo = 0.8 * Vstary[wiersz][kolumna - 1]
                 if wiersz == 0 or mapatypow[wiersz - 1][kolumna] == 0:
                     przemieszczenie_w_gore = 0.1 * Vstary[wiersz][kolumna]
                 else:
@@ -95,11 +104,30 @@ for przejscie in range(1):
                 else:
                     przemieszczenie_w_dol = 0.1 * Vstary[wiersz + 1][kolumna]
                 akcja_lewo = ruch_w_lewo + przemieszczenie_w_gore + przemieszczenie_w_dol
-                print(akcja_lewo)
-
+                #print(akcja_lewo)
+                #wybieranie najepszej akcji
+                if akcja_gora > akcja_prawo:
+                    najlepsza_akcja = akcja_gora
+                else:
+                    najlepsza_akcja = akcja_prawo
+                if najlepsza_akcja < akcja_dol:
+                    najlepsza_akcja = akcja_dol
+                if najlepsza_akcja < akcja_lewo:
+                    najlepsza_akcja = akcja_lewo
+                #Ustalanie najlepszej polityki ruchu
+                if najlepsza_akcja == akcja_gora:
+                    politykaruchu[wiersz][kolumna] = 1
+                elif najlepsza_akcja == akcja_prawo:
+                    politykaruchu[wiersz][kolumna] = 2
+                elif najlepsza_akcja == akcja_dol:
+                    politykaruchu[wiersz][kolumna] = 3
+                elif najlepsza_akcja == akcja_lewo:
+                    politykaruchu[wiersz][kolumna] = 4
+                V[wiersz][kolumna] = mapakosztow[wiersz][kolumna] + gama * najlepsza_akcja
 
 
 
 print(mapatypow)
 print(mapakosztow)
-
+print(politykaruchu)
+print(V)
